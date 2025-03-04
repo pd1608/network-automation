6a2911a4d3f83998ea2795a1ffb2383e5261117e
pd1608 <pranav.deepak1608@gmail.com>
2025-03-04 16:56:40 -0700

    Updated CPU utilization and router data on network-automation repository

--- NMgithub.py
+++ NMgithub.py
@@ -8,13 +8,12 @@ print("current git status: ")
 print(repo.git.status())
 
 repo.index.add(["cpu_utilization.jpg", "router_data.txt"])
-repo.git.add("--all")
 
-repo.index.commit("updated cpu utilization and router data on network-automation repository")
-repo.commit("changes successfully synced")
+repo.git.add('--all')
+
+repo.index.commit("Updated CPU utilization and router data on network-automation repository")
 
 origin = repo.remote(name="origin")
 origin.push()
 
-print("changes updated to Github successfully!!")
-
+print("Changes updated to GitHub successfully!!")
